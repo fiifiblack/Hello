22c4a42d03d671e12ca4eb3c91b11adb060a8ee9
fiifiblack <35601647+fiifiblack@users.noreply.github.com>
2018-05-31 08:11:45 -0400

    entered an date

--- Hello/src/hello/Hello.java
+++ Hello/src/hello/Hello.java
@@ -1,11 +1,13 @@
 package hello;
 
+import java.time.LocalDateTime;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello World");
-
+System.out.println(LocalDateTime.now());
 	}
 
 }
